7cbfe7fcf509904c5ce13098909391b9a1983531
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-06-07 20:00:58 +0200

    added external testing

---
+++ test.js
@@ -0,0 +1,5 @@
+//<script src="sha256.js"></script><script src="test.js"></script>
+
+console.log(Date.now());
+console.log(SHA256("JRBlockkop")); //686327b7352fdfe60892fecfcbd15cf7494022777ac973427409ca3b49d23897
+console.log(Date.now());
